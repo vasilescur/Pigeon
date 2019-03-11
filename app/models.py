582fe7d9b45fcbe68208e09ae09cb8dd5bdd13a2
Radu Vasilescu <vasilescur@gmail.com>
2019-03-11 17:50:59 -0400

    Progress on front page design

--- app/models.py
+++ app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+def article_feature_img_path(instance, filename):
+    return 'feature_images/article_{0}/{1}'.format(instance.id, filename)
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=500, blank=False)
+    tags = models.ManyToManyField(Tag)
+    text = models.TextField(blank=True, null=True, editable=True)
+
+    feature_image = models.ImageField(upload_to=article_feature_img_path, null=True, blank=True)
+
+    date_created = models.DateTimeField(default=timezone.now)
+    creator = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)    # Keep articles of deleted users!
+    
+    is_featured = models.BooleanField(default=False, null=False)
+
+    def __str__(self):
+        return self.headline
+
+    @property
+    def card_desc(self):
+        return ' '.join(self.text.split(' ')[:25]) + '...'
+
+    class Meta:
+        ordering = ('date_created',)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Article
+
+# Front Page!
 def index(request):
-    return HttpResponse("Hello, world!")
+    # Get local articles
+    local_articles = Article.objects.all()[:8]
+
+    # Get featured articles
+    featured_articles = Article.objects.filter(is_featured=True)
+
+    return render(request, 'app/index.html', context = {
+        'articles': local_articles,
+        'featured': featured_articles,
+    })
